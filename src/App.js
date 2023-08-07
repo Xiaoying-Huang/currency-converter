f96566f8f9f896277d624fb2508d2b18ec44ed1d
Xiaoying-Huang <hxybecomexs@gmail.com>
2023-08-07 15:42:59 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [inputCurrency, setInputCurrency] = useState("USD");
+  const [outputCurrency, setOutputCurrency] = useState("EUR");
+  const [convertAmount, setConvertAmount] = useState(1);
+  const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchConvertedCurrency() {
+      setIsLoading(true);
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${convertAmount}&from=${inputCurrency}&to=${outputCurrency}`);
+      const data = await res.json();
+      if (data.message === "not found") {
+        return;
+      }
+      setOutput(current => data["rates"][outputCurrency] + " " + outputCurrency);
+      setIsLoading(false);
+    }
+    if (inputCurrency === outputCurrency) {
+      return setOutput(convertAmount + " " + inputCurrency);
+    }
+    fetchConvertedCurrency();
+  }, [inputCurrency, outputCurrency, convertAmount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={(e) => setConvertAmount(Number(e.target.value))} value={convertAmount} disabled={isLoading} />
+      <select onChange={(e) => setInputCurrency(current => e.target.value)} value={inputCurrency} disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select onChange={(e) => setOutputCurrency(current => e.target.value)} value={outputCurrency} disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{isLoading ? "Loading..." : output}</p>
     </div>
   );
 }
-
-export default App;
